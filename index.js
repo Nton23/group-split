316158f7644a8a3f49c9653c68ce0330bc2189b9
Nton23 <huutonau@gmail.com>
2023-05-18 21:01:04 -0700

    Add function to button/need to work on it

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 //This code block is a function of the result container.
 //It will pop up a window with the result when the submit button is pressed
 //the background will be blur
-const addBtn = document.getElementById('add-book-btn');
-addBtn.addEventListener('click', addClick);
-function addClick() {
-    const formContainer = document.getElementById('form-container');
+const submitBtn = document.getElementById('submit-btn');
+submitBtn.addEventListener('click', submitClick);
+function submitClick() {
+    const resultContainer = document.getElementById('result-container');
     const blurBackground = document.getElementById('blur-background');
-    formContainer.style.display = "block"; 
+    resultContainer.style.display = "block"; 
     blurBackground.style.display = "block";
 }
 
